3550d24b211b01fad24db7246043606a7a001894
Johannes Schindelin <johannes.schindelin@gmx.de>
2013-06-12 16:53:32 -0500

    TODODODO! Build script: Options for debug/rebuild/x64
    The user can now do --debug, --rebuild, and --x64 as they like. Requires a
    commit to follow fixing the solution file configuration mapping.
    
    Signed-off-by: Luke Stuyvenberg <stuyvenberg@wisc.edu>
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    
    Conflicts:
    	build-openspim.sh

--- build-openspim.sh
+++ build-openspim.sh
@@ -18,9 +18,34 @@ bannerdie() {
 	exit 1;
 }
 
+<<<<<<< HEAD
 =======
 >>>>>>> 708944b... Vastly improved build-openspim.sh
 banner "FINDING MY MARBLES D:"
+=======
+platform="Win32"
+config="Release"
+target=
+
+for arg;
+do
+	case $arg in
+	--x64)
+		platform="x64";
+		;;
+	--debug)
+		config="Debug";
+		;;
+	--rebuild)
+		target=":REBUILD";
+		;;
+	*)
+		;;
+	esac;
+done;
+
+banner "FINDING MY MARBLES D:";
+>>>>>>> 546bfc0... Build script: Options for debug/rebuild/x64
 
 # TODO: Set all the correct environment variables; check the VS2010 bat?
 dotnetfwdir=$(reg query HKLM\\SOFTWARE\\Microsoft\\VisualStudio\\SxS\\VC7 //v FrameworkDir32 | grep 'FrameworkDir32' | sed -r -e 's/\s*FrameworkDir32\s*REG_SZ\s*([^\s]*)/\1/g');
@@ -31,31 +56,37 @@ msbuild="$dotnetfwdir$dotnetfwver\\msbuild.exe";
 test -a $msbuild || die "Couldn't find msbuild.exe ($msbuild). No .NET framework?";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "Found msbuild.exe at $msbuild."
+=======
+echo "Found msbuild.exe at $msbuild.";
+>>>>>>> 546bfc0... Build script: Options for debug/rebuild/x64
 
 =======
 >>>>>>> 708944b... Vastly improved build-openspim.sh
 # TODO: Is this recorded anywhere definitive?
-ant="$(pwd)/../3rdpartypublic/apache-ant-1.6.5/bin/ant"
+ant="$(pwd)/../3rdpartypublic/apache-ant-1.6.5/bin/ant";
 
 test -a $ant || die "Couldn't find Apache Ant at $ant. Did you pull 3rdpartypublic?";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "Found ant at $ant."
+=======
+echo "Found ant at $ant.";
+>>>>>>> 546bfc0... Build script: Options for debug/rebuild/x64
 
-banner "BUILDING MMCORE & MMSTUDIO"
+banner "${target:+RE}BUILDING MMCORE & MMSTUDIO IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:MMCore\;MMCoreJ_wrap\;mmstudio //fileLogger1 //verbosity:minimal || bannerdie "FAILED TO BUILD MMCORE/MMSTUDIO! :(";
+# For some reason, you can't specify Build as a target -- you must leave off the target specifier entirely.
 
-test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /property:Configuration=$config /property:Platform=$platform /target:MMCore${target}\;MMCoreJ_wrap${target}\;mmstudio${target} //fileLogger1 //verbosity:minimal && test "$(grep -c '^Build FAILED\.$' msbuild1.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
 
-banner "BUILDING DEVICE ADAPTERS"
+banner "${target:+RE}BUILDING DEVICE ADAPTERS IN $(echo $config | tr '[:lower:]' '[:upper:]') FOR $(echo $platform | tr '[:lower:]' '[:upper:]')";
 
-$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /target:DemoCamera\;TIScam\;QCam\;PicardStage\;SerialManager\;CoherentCube //fileLogger2 //verbosity:minimal || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
+$msbuild MMCoreJ_wrap/MMCoreJ_wrap_v10.sln /property:Configuration=$config /property:Platform=$platform /target:DemoCamera${target}\;TIScam${target}\;QCam${target}\;PicardStage${target}\;SerialManager${target}\;CoherentCube${target} //fileLogger2 //verbosity:minimal && test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
 
-test "$(grep -c '^Build FAILED\.$' msbuild2.log)" == "0" || bannerdie "FAILED TO BUILD DEVICE ADAPTERS! :(";
-
-banner "BUILDING MICRO-MANAGER PLUGINS"
+banner "BUILDING MICRO-MANAGER PLUGINS";
 
 $ant -quiet -buildfile "plugins/build.xml" clean compile build || bannerdie "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(";
 =======
@@ -72,4 +103,4 @@ banner "BUILDING MICRO-MANAGER PLUGINS"
 $ant -quiet -buildfile "plugins/build.xml" clean compile build || banner "FAILED TO BUILD MICRO-MANAGER PLUGINS! :(" | die;
 >>>>>>> 708944b... Vastly improved build-openspim.sh
 
-banner "DONE! :D"
+banner "DONE! :D";
